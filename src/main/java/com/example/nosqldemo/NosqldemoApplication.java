cb1e16dfafe135bbc9e65eea567e43aa5c37f43a
guxianzhao <1092414141@qq.com>
2020-05-23 17:21:11 +0800

    分支代码提交

--- src/main/java/com/example/nosqldemo/NosqldemoApplication.java
+++ src/main/java/com/example/nosqldemo/NosqldemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NosqldemoApplication {
+	// 将代码合并到分支上去
 
 	public static void main(String[] args) {
 		SpringApplication.run(NosqldemoApplication.class, args);
